6e30a46edfd90b62844681af3ae5695dbeb687af
Gourishkumbar <gourishkumbar123@gmail.com>
2025-04-03 20:50:19 +0530

    Updated chatbot backend

--- server.cjs
+++ server.cjs
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import mysql from 'mysql2';
-import dialogflow from '@google-cloud/dialogflow';
+import dialogflow from 'dialogflow';
 
 
 dotenv.config();
